7546570182462cc621dda9662f380f5e3b364467
Aleks Shatokhin <aleks_shatoh@mail.ru>
2022-03-16 00:46:06 +0300

    feat: add profile info

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -8,6 +8,9 @@ import {
   SETUP_USER_ERROR,
   TOGGLE_SIDEBAR,
   LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR,
 } from './actions';
 import reducer from './reducer';
 import axios from 'axios';
@@ -24,13 +27,52 @@ const initialState = {
   userLocation: userLocation || '',
   jobLocation: userLocation || '',
   isToggleSidebar: false,
-  showSidebar: false
+  showSidebar: false,
+  isEditing: false,
+  etiJibId: '',
+  position: '',
+  company: '',
+  jobLocation: userLocation || '',
+  jobTypeOptions: ['full-time', 'part-time', 'remote', 'internship'],
+  jobType: 'full-time',
+  statusOptions: ['interview', 'decline', 'pending'],
+  status: 'pending',
 };
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  // axios.
+  const authFetch = axios.create({
+    baseURL: '/api/v1',
+  });
+
+  //request
+  authFetch.interceptors.request.use(
+    (config) => {
+      config.headers.common['Authorization'] = `Bearer ${state.token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  //response
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response.status === 401) {
+        logoutUser();
+        console.log('AUTH ERROR');
+      }
+      return Promise.reject(error);
+    }
+  );
+
   const displayAlert = () => {
     console.log('dispatch');
     dispatch({ type: DISPLAY_ALERT });
@@ -92,10 +134,42 @@ const AppProvider = ({ children }) => {
     dispatch({ type: LOGOUT_USER });
     removeUserFromLocalStorage();
   };
- 
+  const updateUser = async (currentUser) => {
+    dispatch({ type: UPDATE_USER_BEGIN });
+
+    try {
+      const { data } = await authFetch.patch('/auth/updateUser', currentUser);
+      const { user, location, token } = data;
+      dispatch({
+        type: UPDATE_USER_SUCCESS,
+        payload: {
+          user,
+          token,
+          location,
+        },
+      });
+      addUserToLocalStorage(user, token, location);
+    } catch (err) {
+      if (err.response.status !== 404) {
+        dispatch({
+          type: UPDATE_USER_ERROR,
+          payload: { msg: err.response.data.msg },
+        });
+      }
+    }
+
+    clearAlert();
+  };
   return (
     <AppContext.Provider
-      value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser }}>
+      value={{
+        ...state,
+        displayAlert,
+        setupUser,
+        toggleSidebar,
+        logoutUser,
+        updateUser,
+      }}>
       {children}
     </AppContext.Provider>
   );
